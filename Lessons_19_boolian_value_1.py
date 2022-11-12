0f9495840a1287c6d44d048c2d1f6cf2d11b255a
Volodymyr Rudyk <111973798+VolodymyrRudyk@users.noreply.github.com>
2022-11-12 06:53:13 +0200

    Add files via upload

---
+++ Lessons_19_boolian_value_1.py
@@ -0,0 +1,6 @@
+print(type(True))
+print(isinstance(True, int))
+print(True == 1)                #це те ж саме значення
+print(True is 1)                #но інший об'єкт
+print(True or False)            #те ж саме що і 1 чи 0
+print(True + 4)                 #ooopss...
